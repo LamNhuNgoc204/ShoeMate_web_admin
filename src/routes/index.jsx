d718b05ca98f3ce127f749d640b23db6f318f412
Lam Nhu Ngoc <156599086+LamNhuNgoc204@users.noreply.github.com>
2024-10-09 20:13:22 +0700

    update UI

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 // routes
-import MainRoutes from './MainRoutes';
 import LoginRoutes from './AuthenticationRoutes';
+import MainRoutes from './MainRoutes';
 
 const router = createBrowserRouter([LoginRoutes, MainRoutes], {
   basename: import.meta.env.VITE_APP_BASE_NAME
